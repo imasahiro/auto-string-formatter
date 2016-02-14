4ac869df4a0239262e55e5ba009f8eeaae2b7106
Masahiro Ide <imasahiro9@gmail.com>
2016-02-15 00:25:18 +0900

    Add IntegerFormatter

--- runtime/src/main/java/com/github/imasahiro/stringformatter/runtime/IntegerFormatter.java
+++ runtime/src/main/java/com/github/imasahiro/stringformatter/runtime/IntegerFormatter.java
@@ -3,22 +3,24 @@ package com.github.imasahiro.stringformatter.runtime;
 import com.google.common.annotations.VisibleForTesting;
 
 public class IntegerFormatter {
-    void format(StringBuilder sb, short v) {
+    static public final int PADDED_WITH_ZEROS = 1;
+
+    public static void formatTo(StringBuilder sb, short v, int flags, int width) {
         long unsigned = Math.abs(v);
-        format0(sb, unsigned, v < 0);
+        format0(sb, unsigned, v < 0, flags, width);
     }
 
-    void format(StringBuilder sb, int v) {
+    public static void formatTo(StringBuilder sb, int v, int flags, int width) {
         long unsigned = Math.abs(v);
-        format0(sb, unsigned, v < 0);
+        format0(sb, unsigned, v < 0, flags, width);
     }
 
-    void format(StringBuilder sb, long v) {
+    public static void formatTo(StringBuilder sb, long v, int flags, int width) {
         long unsigned = Math.abs(v);
-        format0(sb, unsigned, v < 0);
+        format0(sb, unsigned, v < 0, flags, width);
     }
 
-    static private final long ILOG10_TABLE[] = {
+    static private final long powersOf10[] = {
             1L,
             10L,
             100L,
@@ -41,21 +43,201 @@ public class IntegerFormatter {
             Long.MAX_VALUE
     };
 
-    @VisibleForTesting
-    static int log2(long unsigned) {
-        return Long.SIZE - Long.numberOfLeadingZeros(unsigned - 1);
-    }
+    // Copied from com.google.common.math.LongMath
+    // maxLog10ForLeadingZeros[i] == floor(log10(2^(Long.SIZE - i)))
+    static private final byte[] maxLog10ForLeadingZeros = {
+            19, 18, 18, 18, 18, 17, 17, 17, 16, 16, 16, 15, 15, 15, 15, 14, 14, 14, 13, 13, 13, 12, 12,
+            12, 12, 11, 11, 11, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4,
+            3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0
+    };
 
-    static private int ilog10ul(long unsigned) {
-        //#define LOG2(N) ((unsigned)((sizeof(long long) * 8) - __builtin_clzll((N)-1)))
-        if (unsigned == 0) {
-            return 1;
+    @VisibleForTesting
+    static int log10(long unsigned) {
+        if (unsigned != 0) {
+            int digits = maxLog10ForLeadingZeros[Long.numberOfLeadingZeros(unsigned)];
+            return digits + (unsigned >= powersOf10[digits] ? 1 : 0);
         } else {
-            int t = ((log2(unsigned) + 1) * 1233) / 4096;
-            return t + (unsigned >= ILOG10_TABLE[t] ? 1 : 0);
+            return 1;
         }
     }
 
-    private void format0(StringBuilder sb, long unsigned, boolean negative) {
+    static private final char digits99[] = {
+            '0', '0', '0', '1', '0', '2', '0', '3', '0', '4', '0', '5', '0', '6', '0', '7', '0', '8', '0', '9',
+            '1', '0', '1', '1', '1', '2', '1', '3', '1', '4', '1', '5', '1', '6', '1', '7', '1', '8', '1', '9',
+            '2', '0', '2', '1', '2', '2', '2', '3', '2', '4', '2', '5', '2', '6', '2', '7', '2', '8', '2', '9',
+            '3', '0', '3', '1', '3', '2', '3', '3', '3', '4', '3', '5', '3', '6', '3', '7', '3', '8', '3', '9',
+            '4', '0', '4', '1', '4', '2', '4', '3', '4', '4', '4', '5', '4', '6', '4', '7', '4', '8', '4', '9',
+            '5', '0', '5', '1', '5', '2', '5', '3', '5', '4', '5', '5', '5', '6', '5', '7', '5', '8', '5', '9',
+            '6', '0', '6', '1', '6', '2', '6', '3', '6', '4', '6', '5', '6', '6', '6', '7', '6', '8', '6', '9',
+            '7', '0', '7', '1', '7', '2', '7', '3', '7', '4', '7', '5', '7', '6', '7', '7', '7', '8', '7', '9',
+            '8', '0', '8', '1', '8', '2', '8', '3', '8', '4', '8', '5', '8', '6', '8', '7', '8', '8', '8', '9',
+            '9', '0', '9', '1', '9', '2', '9', '3', '9', '4', '9', '5', '9', '6', '9', '7', '9', '8', '9', '9'
+    };
+
+    static private void format0(StringBuilder sb, long val, boolean negative, int flags, int width) {
+        int len = log10(val) + (negative ? 1 : 0);
+        if ((flags & PADDED_WITH_ZEROS) != PADDED_WITH_ZEROS) {
+            for (int i = len; i < width; i++) {
+                sb.append(' ');
+            }
+        }
+        if (negative) {
+            sb.append('-');
+        }
+        if ((flags & PADDED_WITH_ZEROS) == PADDED_WITH_ZEROS) {
+            for (int i = len; i < width; i++) {
+                sb.append('0');
+            }
+        }
+        // val = aaaabbbbccccddddeeee
+        if (val <= 99999999) {
+            formatLessThan100Million(sb, val);
+        } else if (val <= 9999999999999999L) {
+            formatLessThan10Quadrillion(sb, val);
+        } else {
+            formatMoreThan10Quadrillion(sb, val);
+        }
+    }
+
+    static private void formatMoreThan10Quadrillion(StringBuilder sb, long val) {
+        // val = aaaabbbbccccddddeeee
+        int a = (int) (val / 10000000000000000L);
+        long bcde = val % 10000000000000000L;
+        int a1 = (a / 100) * 2;
+        int a2 = (a % 100) * 2;
+        int bc = (int) (bcde / 100000000);
+        int de = (int) (bcde % 100000000);
+        int b = bc / 10000;
+        int c = bc % 10000;
+        int d = de / 10000;
+        int e = de % 10000;
+
+        int b1 = (b / 100) * 2;
+        int b2 = (b % 100) * 2;
+        int c1 = (c / 100) * 2;
+        int c2 = (c % 100) * 2;
+        int d1 = (d / 100) * 2;
+        int d2 = (d % 100) * 2;
+        int e1 = (e / 100) * 2;
+        int e2 = (e % 100) * 2;
+
+        if (a >= 1000) {
+            sb.append(digits99[a1]);
+        }
+        if (a >= 100) {
+            sb.append(digits99[a1 + 1]);
+        }
+        if (a >= 10) {
+            sb.append(digits99[a2]);
+        }
+        sb.append(digits99[a2 + 1]);
+        sb.append(digits99[b1]);
+        sb.append(digits99[b1 + 1]);
+        sb.append(digits99[b2]);
+        sb.append(digits99[b2 + 1]);
+        sb.append(digits99[c1]);
+        sb.append(digits99[c1 + 1]);
+        sb.append(digits99[c2]);
+        sb.append(digits99[c2 + 1]);
+        sb.append(digits99[d1]);
+        sb.append(digits99[d1 + 1]);
+        sb.append(digits99[d2]);
+        sb.append(digits99[d2 + 1]);
+        sb.append(digits99[e1]);
+        sb.append(digits99[e1 + 1]);
+        sb.append(digits99[e2]);
+        sb.append(digits99[e2 + 1]);
+    }
+
+    static private void formatLessThan10Quadrillion(StringBuilder sb, long val) {
+        // val = bbbbccccddddeeee
+        long bc = (val / 100000000);
+        long de = (val % 100000000);
+        int b = (int) (bc / 10000);
+        int c = (int) (bc % 10000);
+        int d = (int) (de / 10000);
+        int e = (int) (de % 10000);
+
+        int b1 = (b / 100) * 2;
+        int b2 = (b % 100) * 2;
+        int c1 = (c / 100) * 2;
+        int c2 = (c % 100) * 2;
+        int d1 = (d / 100) * 2;
+        int d2 = (d % 100) * 2;
+        int e1 = (e / 100) * 2;
+        int e2 = (e % 100) * 2;
+        if (bc >= 10000000) {
+            sb.append(digits99[b1]);
+        }
+        if (bc >= 1000000) {
+            sb.append(digits99[b1 + 1]);
+        }
+        if (bc >= 100000) {
+            sb.append(digits99[b2]);
+        }
+        if (bc >= 10000) {
+            sb.append(digits99[b2 + 1]);
+        }
+        if (bc >= 1000) {
+            sb.append(digits99[c1]);
+        }
+        if (bc >= 100) {
+            sb.append(digits99[c1 + 1]);
+        }
+        if (bc >= 10) {
+            sb.append(digits99[c2]);
+        }
+        if (bc >= 1) {
+            sb.append(digits99[c2 + 1]);
+        }
+        sb.append(digits99[d1]);
+        sb.append(digits99[d1 + 1]);
+        sb.append(digits99[d2]);
+        sb.append(digits99[d2 + 1]);
+        sb.append(digits99[e1]);
+        sb.append(digits99[e1 + 1]);
+        sb.append(digits99[e2]);
+        sb.append(digits99[e2 + 1]);
+    }
+
+    private static void formatLessThan100Million(StringBuilder sb, long val) {
+        // val = ddddeeee
+        if (val <= 9999) {
+            // val = eeee
+            int e1 = (int) ((val / 100) * 2);
+            int e2 = (int) ((val % 100) * 2);
+            if (val >= 1000) {
+                sb.append(digits99[e1]);
+            }
+            if (val >= 100) {
+                sb.append(digits99[e1 + 1]);
+            }
+            if (val >= 10) {
+                sb.append(digits99[e2]);
+            }
+            sb.append(digits99[e2 + 1]);
+        } else {
+            // val = ddddeeee
+            int d = (int) (val / 10000);
+            int e = (int) (val % 10000);
+            int d1 = (d / 100) * 2;
+            int d2 = (d % 100) * 2;
+            int e1 = (e / 100) * 2;
+            int e2 = (e % 100) * 2;
+            if (d >= 1000) {
+                sb.append(digits99[d1]);
+            }
+            if (d >= 100) {
+                sb.append(digits99[d1 + 1]);
+            }
+            if (d >= 10) {
+                sb.append(digits99[d2]);
+            }
+            sb.append(digits99[d2 + 1]);
+            sb.append(digits99[e1]);
+            sb.append(digits99[e1 + 1]);
+            sb.append(digits99[e2]);
+            sb.append(digits99[e2 + 1]);
+        }
     }
 }
--- runtime/src/test/java/com/github/imasahiro/stringformatter/runtime/IntegerFormatterTest.java
+++ runtime/src/test/java/com/github/imasahiro/stringformatter/runtime/IntegerFormatterTest.java
@@ -4,31 +4,178 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-class IntegerFormatterTest {
+public class IntegerFormatterTest {
     @Test
     public void testLog2() {
-        assertEquals(1, IntegerFormatter.log2(1));
-        assertEquals(1, IntegerFormatter.log2(9));
+        assertEquals(1, IntegerFormatter.log10(1));
+        assertEquals(1, IntegerFormatter.log10(9));
 
-        assertEquals(2, IntegerFormatter.log2(10));
-        assertEquals(2, IntegerFormatter.log2(11));
-        assertEquals(2, IntegerFormatter.log2(99));
+        assertEquals(2, IntegerFormatter.log10(10));
+        assertEquals(2, IntegerFormatter.log10(11));
+        assertEquals(2, IntegerFormatter.log10(99));
 
-        assertEquals(3, IntegerFormatter.log2(100));
-        assertEquals(3, IntegerFormatter.log2(500));
-        assertEquals(3, IntegerFormatter.log2(999));
+        assertEquals(3, IntegerFormatter.log10(100));
+        assertEquals(3, IntegerFormatter.log10(500));
+        assertEquals(3, IntegerFormatter.log10(999));
 
-        assertEquals(4, IntegerFormatter.log2(1000));
-        assertEquals(4, IntegerFormatter.log2(5000));
-        assertEquals(4, IntegerFormatter.log2(9999));
+        assertEquals(4, IntegerFormatter.log10(1000));
+        assertEquals(4, IntegerFormatter.log10(5000));
+        assertEquals(4, IntegerFormatter.log10(9999));
 
-        assertEquals(5, IntegerFormatter.log2(10000));
-        assertEquals(5, IntegerFormatter.log2(50000));
-        assertEquals(5, IntegerFormatter.log2(99999));
+        assertEquals(5, IntegerFormatter.log10(10000));
+        assertEquals(5, IntegerFormatter.log10(50000));
+        assertEquals(5, IntegerFormatter.log10(99999));
 
-        assertEquals(19, IntegerFormatter.log2(1000000000000000000L));
-        assertEquals(19, IntegerFormatter.log2(5555555555555555555L));
-        assertEquals(19, IntegerFormatter.log2(Long.MAX_VALUE));
-
+        assertEquals(19, IntegerFormatter.log10(1000000000000000000L));
+        assertEquals(19, IntegerFormatter.log10(5555555555555555555L));
+        assertEquals(19, IntegerFormatter.log10(Long.MAX_VALUE));
     }
+
+    private String run(long i, int width, boolean printZero) {
+        StringBuilder sb = new StringBuilder();
+        IntegerFormatter.formatTo(sb, i, printZero ? IntegerFormatter.PADDED_WITH_ZEROS : 0, width);
+        return sb.toString();
+    }
+
+    @Test
+    public void testFormatToLong() {
+        assertEquals(Long.toString(1234567890123456789L), run(1234567890123456789L, 0, false));
+        assertEquals(Long.toString(123456789012345678L), run(123456789012345678L, 0, false));
+        assertEquals(Long.toString(12345678901234567L), run(12345678901234567L, 0, false));
+        assertEquals(Long.toString(1234567890123456L), run(1234567890123456L, 0, false));
+        assertEquals(Long.toString(123456789012345L), run(123456789012345L, 0, false));
+        assertEquals(Long.toString(12345678901234L), run(12345678901234L, 0, false));
+        assertEquals(Long.toString(1234567890123L), run(1234567890123L, 0, false));
+        assertEquals(Long.toString(123456789012L), run(123456789012L, 0, false));
+        assertEquals(Long.toString(12345678901L), run(12345678901L, 0, false));
+        assertEquals(Long.toString(1234567890L), run(1234567890L, 0, false));
+        assertEquals(Long.toString(123456789L), run(123456789L, 0, false));
+        assertEquals(Long.toString(12345678L), run(12345678L, 0, false));
+        assertEquals(Long.toString(1234567L), run(1234567L, 0, false));
+        assertEquals(Long.toString(123456L), run(123456L, 0, false));
+        assertEquals(Long.toString(12345L), run(12345L, 0, false));
+        assertEquals(Long.toString(1234L), run(1234L, 0, false));
+        assertEquals(Long.toString(123L), run(123L, 0, false));
+        assertEquals(Long.toString(12L), run(12L, 0, false));
+        assertEquals(Long.toString(1L), run(1L, 0, false));
+        assertEquals(Long.toString(0L), run(0L, 0, false));
+    }
+
+    @Test
+    public void testFormatToNegativeLong() {
+        assertEquals(Long.toString(-1234567890123456789L), run(-1234567890123456789L, 0, false));
+        assertEquals(Long.toString(-123456789012345678L), run(-123456789012345678L, 0, false));
+        assertEquals(Long.toString(-12345678901234567L), run(-12345678901234567L, 0, false));
+        assertEquals(Long.toString(-1234567890123456L), run(-1234567890123456L, 0, false));
+        assertEquals(Long.toString(-123456789012345L), run(-123456789012345L, 0, false));
+        assertEquals(Long.toString(-12345678901234L), run(-12345678901234L, 0, false));
+        assertEquals(Long.toString(-1234567890123L), run(-1234567890123L, 0, false));
+        assertEquals(Long.toString(-123456789012L), run(-123456789012L, 0, false));
+        assertEquals(Long.toString(-12345678901L), run(-12345678901L, 0, false));
+        assertEquals(Long.toString(-1234567890L), run(-1234567890L, 0, false));
+        assertEquals(Long.toString(-123456789L), run(-123456789L, 0, false));
+        assertEquals(Long.toString(-12345678L), run(-12345678L, 0, false));
+        assertEquals(Long.toString(-1234567L), run(-1234567L, 0, false));
+        assertEquals(Long.toString(-123456L), run(-123456L, 0, false));
+        assertEquals(Long.toString(-12345L), run(-12345L, 0, false));
+        assertEquals(Long.toString(-1234L), run(-1234L, 0, false));
+        assertEquals(Long.toString(-123L), run(-123L, 0, false));
+        assertEquals(Long.toString(-12L), run(-12L, 0, false));
+        assertEquals(Long.toString(-1L), run(-1L, 0, false));
+    }
+
+    @Test
+    public void testFormatToLong_width() {
+        assertEquals(String.format("%30d", 1234567890123456789L), run(1234567890123456789L, 30, false));
+        assertEquals(String.format("%30d", 123456789012345678L), run(123456789012345678L, 30, false));
+        assertEquals(String.format("%30d", 12345678901234567L), run(12345678901234567L, 30, false));
+        assertEquals(String.format("%30d", 1234567890123456L), run(1234567890123456L, 30, false));
+        assertEquals(String.format("%30d", 123456789012345L), run(123456789012345L, 30, false));
+        assertEquals(String.format("%30d", 12345678901234L), run(12345678901234L, 30, false));
+        assertEquals(String.format("%30d", 1234567890123L), run(1234567890123L, 30, false));
+        assertEquals(String.format("%30d", 123456789012L), run(123456789012L, 30, false));
+        assertEquals(String.format("%30d", 12345678901L), run(12345678901L, 30, false));
+        assertEquals(String.format("%30d", 1234567890L), run(1234567890L, 30, false));
+        assertEquals(String.format("%30d", 123456789L), run(123456789L, 30, false));
+        assertEquals(String.format("%30d", 12345678L), run(12345678L, 30, false));
+        assertEquals(String.format("%30d", 1234567L), run(1234567L, 30, false));
+        assertEquals(String.format("%30d", 123456L), run(123456L, 30, false));
+        assertEquals(String.format("%30d", 12345L), run(12345L, 30, false));
+        assertEquals(String.format("%30d", 1234L), run(1234L, 30, false));
+        assertEquals(String.format("%30d", 123L), run(123L, 30, false));
+        assertEquals(String.format("%30d", 12L), run(12L, 30, false));
+        assertEquals(String.format("%30d", 1L), run(1L, 30, false));
+        assertEquals(String.format("%30d", 0L), run(0L, 30, false));
+    }
+
+    @Test
+    public void testFormatToNegativeLong_width() {
+        assertEquals(String.format("%30d", -1234567890123456789L), run(-1234567890123456789L, 30, false));
+        assertEquals(String.format("%30d", -123456789012345678L), run(-123456789012345678L, 30, false));
+        assertEquals(String.format("%30d", -12345678901234567L), run(-12345678901234567L, 30, false));
+        assertEquals(String.format("%30d", -1234567890123456L), run(-1234567890123456L, 30, false));
+        assertEquals(String.format("%30d", -123456789012345L), run(-123456789012345L, 30, false));
+        assertEquals(String.format("%30d", -12345678901234L), run(-12345678901234L, 30, false));
+        assertEquals(String.format("%30d", -1234567890123L), run(-1234567890123L, 30, false));
+        assertEquals(String.format("%30d", -123456789012L), run(-123456789012L, 30, false));
+        assertEquals(String.format("%30d", -12345678901L), run(-12345678901L, 30, false));
+        assertEquals(String.format("%30d", -1234567890L), run(-1234567890L, 30, false));
+        assertEquals(String.format("%30d", -123456789L), run(-123456789L, 30, false));
+        assertEquals(String.format("%30d", -12345678L), run(-12345678L, 30, false));
+        assertEquals(String.format("%30d", -1234567L), run(-1234567L, 30, false));
+        assertEquals(String.format("%30d", -123456L), run(-123456L, 30, false));
+        assertEquals(String.format("%30d", -12345L), run(-12345L, 30, false));
+        assertEquals(String.format("%30d", -1234L), run(-1234L, 30, false));
+        assertEquals(String.format("%30d", -123L), run(-123L, 30, false));
+        assertEquals(String.format("%30d", -12L), run(-12L, 30, false));
+        assertEquals(String.format("%30d", -1L), run(-1L, 30, false));
+    }
+
+    @Test
+    public void testFormatToLong_width_padded_with_zero() {
+        assertEquals(String.format("%030d", 1234567890123456789L), run(1234567890123456789L, 30, true));
+        assertEquals(String.format("%030d", 123456789012345678L), run(123456789012345678L, 30, true));
+        assertEquals(String.format("%030d", 12345678901234567L), run(12345678901234567L, 30, true));
+        assertEquals(String.format("%030d", 1234567890123456L), run(1234567890123456L, 30, true));
+        assertEquals(String.format("%030d", 123456789012345L), run(123456789012345L, 30, true));
+        assertEquals(String.format("%030d", 12345678901234L), run(12345678901234L, 30, true));
+        assertEquals(String.format("%030d", 1234567890123L), run(1234567890123L, 30, true));
+        assertEquals(String.format("%030d", 123456789012L), run(123456789012L, 30, true));
+        assertEquals(String.format("%030d", 12345678901L), run(12345678901L, 30, true));
+        assertEquals(String.format("%030d", 1234567890L), run(1234567890L, 30, true));
+        assertEquals(String.format("%030d", 123456789L), run(123456789L, 30, true));
+        assertEquals(String.format("%030d", 12345678L), run(12345678L, 30, true));
+        assertEquals(String.format("%030d", 1234567L), run(1234567L, 30, true));
+        assertEquals(String.format("%030d", 123456L), run(123456L, 30, true));
+        assertEquals(String.format("%030d", 12345L), run(12345L, 30, true));
+        assertEquals(String.format("%030d", 1234L), run(1234L, 30, true));
+        assertEquals(String.format("%030d", 123L), run(123L, 30, true));
+        assertEquals(String.format("%030d", 12L), run(12L, 30, true));
+        assertEquals(String.format("%030d", 1L), run(1L, 30, true));
+        assertEquals(String.format("%030d", 0L), run(0L, 30, true));
+    }
+
+    @Test
+    public void testFormatToNegativeLong_width_padded_with_zero() {
+        assertEquals(String.format("%030d", -1234567890123456789L), run(-1234567890123456789L, 30, true));
+        assertEquals(String.format("%030d", -123456789012345678L), run(-123456789012345678L, 30, true));
+        assertEquals(String.format("%030d", -12345678901234567L), run(-12345678901234567L, 30, true));
+        assertEquals(String.format("%030d", -1234567890123456L), run(-1234567890123456L, 30, true));
+        assertEquals(String.format("%030d", -123456789012345L), run(-123456789012345L, 30, true));
+        assertEquals(String.format("%030d", -12345678901234L), run(-12345678901234L, 30, true));
+        assertEquals(String.format("%030d", -1234567890123L), run(-1234567890123L, 30, true));
+        assertEquals(String.format("%030d", -123456789012L), run(-123456789012L, 30, true));
+        assertEquals(String.format("%030d", -12345678901L), run(-12345678901L, 30, true));
+        assertEquals(String.format("%030d", -1234567890L), run(-1234567890L, 30, true));
+        assertEquals(String.format("%030d", -123456789L), run(-123456789L, 30, true));
+        assertEquals(String.format("%030d", -12345678L), run(-12345678L, 30, true));
+        assertEquals(String.format("%030d", -1234567L), run(-1234567L, 30, true));
+        assertEquals(String.format("%030d", -123456L), run(-123456L, 30, true));
+        assertEquals(String.format("%030d", -12345L), run(-12345L, 30, true));
+        assertEquals(String.format("%030d", -1234L), run(-1234L, 30, true));
+        assertEquals(String.format("%030d", -123L), run(-123L, 30, true));
+        assertEquals(String.format("%030d", -12L), run(-12L, 30, true));
+        assertEquals(String.format("%030d", -1L), run(-1L, 30, true));
+    }
+
 }
