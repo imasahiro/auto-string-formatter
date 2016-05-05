55630f806a44ec95677a63aecbb464997ef1ea9b
Masahiro Ide <imasahiro9@gmail.com>
2016-05-06 02:37:56 +0900

    Fix release.mk

--- release.mk
+++ release.mk
@@ -1,9 +1,9 @@
 
 check:
+	echo ${bintrayUser}
+	echo ${bintrayKey}
 	./gradlew verifyRelease
 
 release: check
-	./gradlew release
-	./gradlew -PdryRun=false -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey runtime:bintrayUpload
-	./gradlew -PdryRun=false -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey processor:bintrayUpload
-	./gradlew publish
+	./gradlew -PdryRun=false -PbintrayUser=${bintrayUser} -PbintrayKey=${bintrayKey} runtime:bintrayUpload
+	./gradlew -PdryRun=false -PbintrayUser=${bintrayUser} -PbintrayKey=${bintrayKey} processor:bintrayUpload
