ee5abd56946f1f2d6da935ad5b994ee8c027b15b
Manabu Matsuzaki <matsumana@gmail.com>
2019-01-17 17:20:56 +0900

    Migrate to the JDKs managed by Travis-CI

--- test/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-cd $(dirname $0)
-
-JAVA_HOME=$HOME/.jdk/default_8  ./gradlew --no-daemon --stacktrace clean build && $HOME/.jdk/default_8/bin/java  -jar ./build/libs/auto-string-formatter-test-*.jar
-JAVA_HOME=$HOME/.jdk/default_11 ./gradlew --no-daemon --stacktrace clean build && $HOME/.jdk/default_11/bin/java -jar ./build/libs/auto-string-formatter-test-*.jar
