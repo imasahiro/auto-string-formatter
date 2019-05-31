10f1240a549e5711ace9915747f0520fc0a3f3f7
Masahiro Ide <masahiro.ide@linecorp.com>
2019-05-31 16:19:57 +0900

    Cleanup unused release plugin

--- release.mk
+++ release.mk
@@ -2,7 +2,8 @@
 check:
 	echo ${bintrayUser}
 	echo ${bintrayKey}
-	./gradlew verifyRelease
+	./gradlew -PdryRun=false -PbintrayUser=${bintrayUser} -PbintrayKey=${bintrayKey} -PdryRun=true runtime:bintrayUpload
+	./gradlew -PdryRun=false -PbintrayUser=${bintrayUser} -PbintrayKey=${bintrayKey} -PdryRun=true processor:bintrayUpload
 
 release: check
 	./gradlew -PdryRun=false -PbintrayUser=${bintrayUser} -PbintrayKey=${bintrayKey} runtime:bintrayUpload
